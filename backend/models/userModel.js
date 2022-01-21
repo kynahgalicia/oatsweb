a404a94c94031aad3ed0ebf615aa915bf6e2dacf
VinceMichaelLingon <vincemichaellingon@gmail.com>
2022-01-22 04:35:15 +0800

    models lang muna may problem ako sa pagrun ng derver hehe

---
+++ backend/models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    user_tupid: {
+        type: String,
+        required: [true, "Please enter your TUP ID!"],
+        trim: true,
+        unique: true
+    },
+    user_fname: {
+        type: String,
+        required: [true, "Please enter your First name!"],
+        trim: true
+        
+    },
+    user_lname: {
+        type: String,
+        required: [true, "Please enter your Last name!"],
+        trim: true
+        
+    },
+    user_contact: {
+        type: Number,
+        required: [true, "Please enter your Contact Number!"],
+        trim: true,
+        unique: true
+        
+    },
+    user_tupmail: {
+        type: String,
+        required: [true, "Please enter your TUP Email!"],
+        trim: true,
+        unique: true
+    },
+    user_password: {
+        type: String,
+        required: [true, "Please enter your password!"]
+    },
+    user_section: {
+        type: String,
+        required: [true, "Please enter your Section!"],
+        trim: true
+        
+    },
+    avatar: {
+        type: String,
+        default: ""
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Users", userSchema)
