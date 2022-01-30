1719a2927d35147ce3df19646c8be9d17130e7dd
kynahgalicia <70066733+kynahgalicia@users.noreply.github.com>
2022-01-30 21:41:09 +0800

    Update adminModel.js

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -4,23 +4,24 @@ const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
 
 const adminSchema = new mongoose.Schema({
-    atupid: {type: Number, required: true, minlength: 10, maxlength: 10},
-    afname:{type: String, required: true, minlength: 2, maxlength: 30 },
-    alname:{type: String, required: true, minlength: 2, maxlength: 30 },
-    acontact:{type: Number, required: true, minlength:11, maxlength: 11},
-    atupmail: {type: String, required: true, unique: true, minlength:10, maxlength:30},
-    apassword: {type: String, required:true, minlength:6},
-    admintype:{type: String, required: true,
+    atupid: {type: Number, required: true, trim:true, minlength: 10, maxlength: 10},
+    afname:{type: String, required: true, trim:true, minlength: 2, maxlength: 30 },
+    alname:{type: String, required: true, trim:true, minlength: 2, maxlength: 30 },
+    acontact:{type: Number, required: true, trim:true, minlength:11, maxlength: 11},
+    atupmail: {type: String, required: true, trim:true, unique: true, minlength:10, maxlength:30},
+    apassword: {type: String, required:true, trim:true, minlength:6},
+    admintype:{type: String, required: true, trim:true,
         enum: {
             values: [
                 'Faculty',
                 'Officer'
             ],
-            message: 'Please select your admin type'
+            message: 'Please select your admin type',
+            trim:true
         }},
     deptlist: [{
         dept: {type: mongoose.Schema.ObjectId, ref: 'Department',required: true},
-        deptname: {type: String, required: true}
+        deptname: {type: String, trim:true, required: true}
     }],
     avatar: [{
         public_id: {type: String,required: true},
