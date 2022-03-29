b37c8b3a7bddbd25ff660da7ea4eba1b8d56f59a
LeslieAyacocho <ayacocho.leslie@gmail.com>
2022-03-29 18:00:09 +0800

    package/ server

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ app.use(fileUpload({
     useTempFiles: true
 }))
 
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 
 app.use(cors());
 app.use(express.json());
