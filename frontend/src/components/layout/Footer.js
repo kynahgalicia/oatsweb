dffdffb3ea7971aa6d3cd15a59cf52fbc83d6259
LeslieAyacocho <ayacocho.leslie@gmail.com>
2022-05-24 18:07:37 +0800

    Footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,10 +1,12 @@
 import logo from '../img/logo.png'
+import {Link} from 'react-router-dom'
 const Footer = () => {
     return ( 
         <div>
             <div className="left">
             
             <h2 className="d-inline"> <img src={logo} alt="logo" className="img-logo d-inline mx-2 my-1" /> Online Archiving Thesis System (OATS)</h2>
+            
             <p>Technological University of the Philippines - Taguig</p>
             <p>Km. 14 East Service Road, Western Bicutan, Taguig, 1630, Philippines</p>
             <p>(02) 823 2457</p>
@@ -14,6 +16,8 @@ const Footer = () => {
             
             <div className="right">
                 <p>Copyright 2021</p>
+
+                <Link to='/terms' > <p>Terms and Conditions</p></Link>
                 
                 <p>Online Thesis Archiving System (OATS)</p>
                 <p>Technological University of the Philippines - Taguig</p>
